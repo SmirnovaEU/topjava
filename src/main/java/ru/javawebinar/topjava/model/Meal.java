53dd5799ad2f1ee26ceafa8bd39834a03f93a18b
SmirnovaEU <62511446+SmirnovaEU@users.noreply.github.com>
2020-10-13 20:01:56 +0300

    HW02 v.1

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -7,7 +7,6 @@ import java.time.LocalDateTime;
 import java.time.LocalTime;
 @Component
 public class Meal extends AbstractBaseEntity{
-    private Integer id;
 
     private final LocalDateTime dateTime;
 
@@ -28,14 +27,6 @@ public class Meal extends AbstractBaseEntity{
         this.description = description;
         this.calories = calories;
         this.userId = userId;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
     }
 
     public LocalDateTime getDateTime() {
