da96e27797acaec5c7a16f1b183e2b0b2a900610
SmirnovaEU <62511446+SmirnovaEU@users.noreply.github.com>
2020-10-20 21:06:55 +0300

    HW03 base task

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,3 +1,4 @@
+DELETE FROM meals;
 DELETE FROM user_roles;
 DELETE FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
@@ -9,3 +10,12 @@ VALUES ('User', 'user@yandex.ru', 'password'),
 INSERT INTO user_roles (role, user_id)
 VALUES ('USER', 100000),
        ('ADMIN', 100001);
+
+INSERT INTO meals (date_time, description, calories, user_id)
+VALUES ('2020-10-1 10:00', 'Завтрак', 500, 100000),
+       ('2020-10-1 15:00', 'Обед', 1000, 100000),
+       ('2020-10-1 20:00', 'Ужин', 500, 100000),
+       ('2020-10-1 10:00', 'Завтрак', 1000, 100001),
+       ('2020-10-1 13:00', 'Обед', 500, 100001),
+       ('2020-10-1 18:00', 'Ужин', 410, 100001);
+
